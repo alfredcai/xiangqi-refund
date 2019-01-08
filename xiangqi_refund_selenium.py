0f20c317d18e659087b8bd1d052f639a5885d61c
ShAlfred <alfredcai@outlook.com>
2019-01-08 18:12:22 +0800

    add schedule fuction

--- xiangqi_refund_selenium.py
+++ xiangqi_refund_selenium.py
@@ -1,18 +1,31 @@
 # -*- coding: utf-8 -*-
-import datetime
 import logging
 import sys
 import time
+from datetime import datetime
 
 from selenium import webdriver
 
 HOST = "http://gis1z4xshb2s37ki.mikecrm.com"
 
 
-def do_request(username, mobile):
-    logging.info(
-        "request form with username:%s, mobile:%s" % (username, mobile))
-    do_chrome_submit(username, mobile)
+def do_request(username, mobile, time="09:00:00"):
+    while True:
+        if check_request_time(time):
+            logging.info("starting form at %s with username:%s, mobile:%s" % (
+                str(datetime.now().strftime("%Y-%m-%d %H:%M:%S")), username, mobile))
+            do_chrome_submit(username, mobile)
+            return 0
+
+
+def check_request_time(time="9:00:00"):
+    now = datetime.now()
+    settled_time = datetime.strptime(time, "%H:%M:%S")
+    settled_time = now.replace(hour=settled_time.time().hour,
+                               minute=settled_time.time().minute,
+                               second=settled_time.time().second,
+                               microsecond=0)
+    return now > settled_time
 
 
 def do_chrome_submit(username, mobile):
@@ -29,7 +42,9 @@ def do_chrome_submit(username, mobile):
     name_input.send_keys(username)
     mobile_input.send_keys(mobile)
     submit_button.click()
-    logging.info("submit button click with username:%s, mobile:%s" % (username, mobile))
+    logging.info(
+        "########################  submit button click with username:%s, mobile:%s"
+        % (username, mobile))
 
     check_chrome_submit_result(client)
     client.quit()
@@ -42,6 +57,8 @@ def get_web_form(client, url):
         name_input = inputs[0]
         mobile_input = inputs[1]
         submit_button = client.find_element_by_id("form_submit")
+
+        logging.info("get name,mobile input and submit button")
     except Exception as e:
         logging.error(e)
         logging.error("can't find web page's elements, pls check the page url")
@@ -64,7 +81,7 @@ def get_chrome_options():
 
 def check_chrome_submit_result(client):
     logging.info("check ing submit result %s" % str(
-        datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")))
+        datetime.now().strftime("%Y-%m-%d %H:%M:%S")))
     time.sleep(5)
     try:
         result = client.find_element_by_xpath(
@@ -73,21 +90,23 @@ def check_chrome_submit_result(client):
         logging.error(e)
 
     if result:
-        logging.info("submit result:" + result)
+        logging.info("******************* submit result:" + result)
     else:
-        logging.warning("submit result:" + result)
-        logging.WARNING("submit failed")
+        logging.warning("******************* submit result:" + result)
+        logging.WARNING("******************* submit failed")
 
 
 def get_file_path():
     system = sys.platform
     logging.info("system platform is " + system)
-    return {
-        "darwin": ("/Users/alfredcai/CodeProjects/ReleasesProgram/selenium/chromedriver",
-                   "./logger.log"),
-        "linux": ("/root/xiangqi/chromedriver", "/root/xiangqi/log/xiangqi.log")
-    }.get(system, ("/Users/alfredcai/CodeProjects/ReleasesProgram/selenium/chromedriver",
-                   "./logger.log"))
+    config_dir = {
+        "darwin":
+            ("/Users/alfredcai/CodeProjects/ReleasesProgram/selenium/chromedriver",
+             "./logger.log"),
+        "linux": ("/root/xiangqi/chromedriver",
+                  "/root/xiangqi/log/xiangqi.log")
+    }
+    return config_dir.get(system, config_dir.get("darwin"))
 
 
 def set_logging_config(log_file_path):
@@ -103,21 +122,25 @@ def set_logging_config(log_file_path):
 
     console = logging.StreamHandler()
     console.setLevel(logging.DEBUG)
-    console.setFormatter(logging.Formatter(fmt=fmt,datefmt=datefmt))
+    console.setFormatter(logging.Formatter(fmt=fmt, datefmt=datefmt))
     logging.getLogger("").addHandler(console)
 
 
 if __name__ == '__main__':
     CHROME_DRIVER_PATH, LOGGER_FILE_PATH = get_file_path()
     set_logging_config(LOGGER_FILE_PATH)
+
     logging.info("chrome driver path:%s" % CHROME_DRIVER_PATH)
     logging.info("logging file locale at:%s" % LOGGER_FILE_PATH)
 
-    arg_username = str(sys.argv[1])
-    arg_mobile = str(sys.argv[2])
+    try:
+        arg_username = str(sys.argv[1])
+        arg_mobile = str(sys.argv[2])
+        arg_request_time = str(sys.argv[3])
+    except IndexError as e:
+        logging.error(e)
+        sys.exit(0)
 
-    logging.info("args lens: %d" % len(sys.argv))
-    logging.info("args username: %s, mobile: %s" % (arg_username, arg_mobile))
-    logging.info("do request at %s" % str(
-        datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")))
-    do_request(arg_username, arg_mobile)
+    logging.info("args lens:%d, detail:%s" % (len(sys.argv), sys.argv))
+
+    do_request(arg_username, arg_mobile, arg_request_time)
